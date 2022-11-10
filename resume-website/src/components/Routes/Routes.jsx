eb85f7349319296d6b1b1321a72f3cddf34efdc7
dom <samuelmebersole@gmail.com>
2022-11-10 10:11:34 -0500

    Contact page added; redirect on '/' added

--- resume-website/src/components/Routes/Routes.jsx
+++ resume-website/src/components/Routes/Routes.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import {
   Routes as Switch,
   Route,
+  Navigate,
 } from 'react-router-dom';
 import ProjectsPage from '../ProjectsPage/ProjectsPage';
+import {
+  HOME_URL,
+  PROJECTS_URL,
+  CONTACT_URL,
+} from '../../constants.js';
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/Projects" element={<ProjectsPage />} />
+      <Route path='/' element={ <Navigate to={HOME_URL} /> } />
+      <Route path={HOME_URL} element={<h1>Home</h1>} />
+      <Route path={PROJECTS_URL} element={<ProjectsPage />} />
+      <Route path={CONTACT_URL} element={<ProjectsPage />} />
     </Switch>
   );
 }
---
+++ resume-website/src/constants.js
@@ -0,0 +1,19 @@
+// URLS
+const BASE_URL = '/resume-website';
+const HOME_URL = BASE_URL;
+const PROJECTS_URL = BASE_URL + '/Projects';
+const CONTACT_URL = BASE_URL + '/Contact';
+
+const PAGES = [
+  { link: HOME_URL, text: 'Home' },
+  { link: PROJECTS_URL, text: 'Projects' },
+  { link: CONTACT_URL, text: 'Contact' },
+];
+
+export {
+  BASE_URL,
+  HOME_URL,
+  PROJECTS_URL,
+  CONTACT_URL,
+  PAGES,
+};
