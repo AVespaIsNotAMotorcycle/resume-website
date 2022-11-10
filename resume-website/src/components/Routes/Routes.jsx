f5229e45624ad515d24af24ad442294e815f0228
dom <samuelmebersole@gmail.com>
2022-11-10 10:17:18 -0500

    <Home> now in <Routes>

--- resume-website/src/components/Routes/Routes.jsx
+++ resume-website/src/components/Routes/Routes.jsx
@@ -4,7 +4,12 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+
+// High-level components
+import Home from '../Home/Home';
 import ProjectsPage from '../ProjectsPage/ProjectsPage';
+
+// URL constants
 import {
   HOME_URL,
   PROJECTS_URL,
@@ -15,7 +20,7 @@ function Routes() {
   return (
     <Switch>
       <Route path='/' element={ <Navigate to={HOME_URL} /> } />
-      <Route path={HOME_URL} element={<h1>Home</h1>} />
+      <Route path={HOME_URL} element={<Home />} />
       <Route path={PROJECTS_URL} element={<ProjectsPage />} />
       <Route path={CONTACT_URL} element={<ProjectsPage />} />
     </Switch>
