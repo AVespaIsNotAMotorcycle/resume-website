d627dc22801b933f73e96ef7a77399eec8b8678d
dom <samuelmebersole@gmail.com>
2022-11-15 07:41:22 -0500

    Updated homepage text

--- resume-website/src/components/Home/constants.jsx
+++ resume-website/src/components/Home/constants.jsx
@@ -1,12 +1,11 @@
 const ABOUT = `
-Welcome to my website! My name is Sam Ebersole, and I am a Full-Stack Developer currently living in New York City, USA, though I am moving to Berlin, Germany this winter.
-
-
 In May 2022 I graduated cum laude from Hunter College with a Bachelor's of Arts. My majors were Computer Science and History, and I minored in Mathematics and German. While in college, I worked primarily with C++ as part of the Hunter CS program's mains series of courses. I additionally gained experience with Python, especially during my Artificial Intelligence course, and with React, during both my Intro to Modern Web Development course and my capstone project.
 
 
-After graduating, I began work at DataMixMaster, where I am a frontend developer primarily using React. In addition to React, I also use Flask and MongoDB when I have to work on the backend. As a frontend developer, I write automated tests, review and debug code, and implement new features. During my time at DataMixMaster, new features that I implemented have been successfully demoed to customers, resulting in the securing of higher contracts.
+After graduating, I began work at DataMixMaster, where I am a frontend developer primarily using React. In addition to React, I also have limited exposure to Flask and MongoDB, as I run our backend locally when developing to avoid unnecessary changes to production. As a frontend developer following a CI/CD methodology, I write automated tests for new and existing code to ensure stability, develop and deploy new features, and review and maintain an existing codebase. While working at DataMixMaster, features I implemented have been used to secure higher contracts from clients.
 
+
+Feel free to check out my projects on this site or on my Github, and send me an email if you think I'd be right for your team!
 
 ### Links
 - Email: [samuelmebersole@gmail.com](mailto:samuelmebersole@gmail.com)
