8a5d70e6f33e4dfc2d60b813c5698abf783c176c
dom <samuelmebersole@gmail.com>
2022-11-14 19:09:24 -0500

    Styling Home

--- resume-website/src/components/Home/Home.jsx
+++ resume-website/src/components/Home/Home.jsx
@@ -3,24 +3,47 @@ import markdownParser from '../Common/MarkdownParser';
 import ABOUT from './constants';
 import HEADSHOT from './headshot.JPG';
 import './Home.css';
+import { MOBILE } from '../../constants';
 
 function Home() {
   const desc = markdownParser(ABOUT);
 
+  if (MOBILE) {
+    return (
+      <div className="home">
+        <div>
+          <div className="home-container-mobile">
+            <div className="home-container-child">
+              <img className="home-headshot" src={HEADSHOT} alt="headshot" />
+            </div>
+            <div className="home-container-child">
+              <h1>Sam Ebersole</h1>
+              <h2>Frontend Developer at DataMixMaster</h2>
+            </div>
+          </div>
+          <div className="home-container-text">
+            {desc}
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="home">
       <div className="home-padding" />
-      <div className="home-container">
-        <div className="home-container-child">
-          <img className="home-headshot" src={HEADSHOT} alt="headshot" />
+      <div>
+        <div className="home-container-web">
+          <div className="home-container-child">
+            <img className="home-headshot" src={HEADSHOT} alt="headshot" />
+          </div>
+          <div className="home-container-child">
+            <h1>Sam Ebersole</h1>
+            <h2>Frontend Developer at DataMixMaster</h2>
+          </div>
         </div>
-        <div className="home-container-child">
-          <h1>Sam Ebersole</h1>
-          <h2>Frontend Developer at DataMixMaster</h2>
+        <div className="home-container-text">
+          {desc}
         </div>
-      </div>
-      <div className="home-container">
-        {desc}
       </div>
       <div className="home-padding" />
     </div>
