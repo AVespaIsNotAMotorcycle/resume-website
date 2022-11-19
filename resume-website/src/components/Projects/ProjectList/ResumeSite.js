03294ebdf2f82c6e511a20b47d3678bcf83b3de1
dom <samuelmebersole@gmail.com>
2022-11-19 11:19:26 -0500

    Update ResumeSite.js

--- resume-website/src/components/Projects/ProjectList/ResumeSite.js
+++ resume-website/src/components/Projects/ProjectList/ResumeSite.js
@@ -2,7 +2,16 @@ const RESUMESITE = {
   name: 'Resume Site',
   id: 'ResumeSite',
   desc: 'This website!',
-  body: '',
+  body: `
+# Resume Site
+[View the Github repo](https://github.com/AVespaIsNotAMotorcycle/resume-website)
+
+
+A single-page static site built with React and served via Github Pages. During development I used Jest for unit testing to ensure that no changes caused errors or warnings. Pushes to Github were managed via a makefile which ran tests first, ensuring that all pushes consisted only of changes which passed all tests. Admittedly, given the simplicity of the site, this was a little overkill, but I wanted to show that I could.
+
+
+One interesting interaction I observed was between Jest and two markdown parsers (react-markdown and remarkable), in that both cause warnings on unit tests involving markdown. My friend (@mthrndr on Github) had built an incomplete markdown parser for another project, so with his permission I borrowed and expanded it.
+`,
 };
 
 export default RESUMESITE;
