2c84d1481c8d1873ac60076ee566c29698503513
sam <samuelmebersole@gmail.com>
2022-11-25 15:12:17 -0500

    Added entry for PyChess

---
+++ resume-website/src/components/Projects/ProjectList/PyChess.js
@@ -0,0 +1,16 @@
+const PYCHESS = {
+  name: 'PyChess',
+  id: 'PyChess',
+  desc: 'A CLI program for playing chess, either against another person or the computer.',
+  body: `
+# PyChess
+
+[View the Github repo](https://github.com/AVespaIsNotAMotorcycle/python-chess)
+
+
+PyChess is a simple command line interface for playing chess. You can play against another person, taking turns at the same keyboard, or against simple AIs. You can also pit AIs against each other and watch the results. At present, there is only one AI, which makes moves at random, but I intend to expand that list.
+
+![Sreenshot of a game against the AI](${process.env.PUBLIC_URL}/assets/images/pychess-against-ai.png)`,
+};
+
+export default PYCHESS;
--- resume-website/src/components/Projects/constants.jsx
+++ resume-website/src/components/Projects/constants.jsx
@@ -1,11 +1,13 @@
 import GROOVY from './ProjectList/Groovy';
 import LDA from './ProjectList/LDA';
 import RESUMESITE from './ProjectList/ResumeSite';
+import PYCHESS from './ProjectList/PyChess';
 
 const PROJECT_LIST = {
   Groovy: { ...GROOVY },
   LDA: { ...LDA },
   ResumeSite: { ...RESUMESITE },
+  PyChess: { ...PYCHESS },
 };
 
 export default PROJECT_LIST;
