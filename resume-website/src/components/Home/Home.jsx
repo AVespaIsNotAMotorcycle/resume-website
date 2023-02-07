397434b44b5e2c89481ed17d348b75b2553a9a77
sam <samuelmebersole@gmail.com>
2023-02-07 10:06:35 +0100

    Revamped homepage

--- resume-website/src/components/Home/Home.jsx
+++ resume-website/src/components/Home/Home.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
 import markdownParser from '../Common/MarkdownParser';
 import ABOUT from './constants';
-import HEADSHOT from './headshot.JPG';
 import './Home.css';
-import { MOBILE } from '../../constants';
 
 function Home() {
   const desc = markdownParser(ABOUT);
 
-  if (MOBILE) {
-    return (
-      <div className="home">
-        <div>
-          <div className="home-container-mobile">
-            <div className="home-container-child">
-              <img className="home-headshot" src={HEADSHOT} alt="headshot" />
-            </div>
-            <div className="home-container-child">
-              <h1>Sam Ebersole</h1>
-              <h2>Frontend Developer at DataMixMaster</h2>
-            </div>
-          </div>
-          <div className="home-container-text">
-            {desc}
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
-    <div className="home">
+    <div className="home" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/home-background.JPG)` }}>
       <div className="home-padding" />
-      <div>
+      <div className="home-padding" />
+      <div className="home-padding home-color-fade" />
+      <div className="home-content">
         <div className="home-container-web">
-          <div className="home-container-child">
-            <img className="home-headshot" src={HEADSHOT} alt="headshot" />
-          </div>
           <div className="home-container-child">
             <h1>Sam Ebersole</h1>
             <h2>Frontend Developer at DataMixMaster</h2>
@@ -45,7 +22,6 @@ function Home() {
           {desc}
         </div>
       </div>
-      <div className="home-padding" />
     </div>
   );
 }
