05b500a30ca220c79e1917dc70c21df78a459078
sam <samuelmebersole@gmail.com>
2023-03-18 09:55:43 +0100

    Added links to NoviiMD page

--- resume-website/src/components/Projects/ProjectList/NoviiMD.js
+++ resume-website/src/components/Projects/ProjectList/NoviiMD.js
@@ -2,7 +2,13 @@ const NOVIIMD = {
   name: 'NoviiMD',
   id: 'NoviiMD',
   desc: 'A markdown parser for React, published as an npm package.',
-  body: 'A markdown parser written in React with an editor hosted via GitHub Pages. Try it out at http://noviimd.samuelmebersole.com/ or install with `npm install noviimd`.',
+  body: `
+# NoviiMD
+[Online demo](http://noviimd.samuelmebersole.com/)
+
+[View the GitHub repo](https://github.com/AVespaIsNotAMotorcycle/noviimd)
+
+A markdown parser written in React with an editor hosted via GitHub Pages. Try it out at http://noviimd.samuelmebersole.com/ or install with 'npm install noviimd'.`,
   tags: [
     'React.js',
     'Jest',
